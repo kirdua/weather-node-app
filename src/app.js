df98a469d8d1a12904912ebe71a0f1a288dd1afd
kirdua <kirdua@gmail.com>
2021-04-19 14:16:40 -0500

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -87,6 +88,9 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(5000, () => {
-    console.log('Server running on 5000')
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
 })
